6ea9c947b1bb983f898411cc7bafb1dcb48fb4be
hoangngocthang <>
2023-02-12 16:38:22 +0700

    cm

--- lib/sceens/home-screen.dart
+++ lib/sceens/home-screen.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final String nameTodo = "";
+  bool show = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,47 +23,50 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-                margin: EdgeInsets.all(15),
-                height: 120,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Flexible(
-                      fit: FlexFit.tight,
-                      child: SizedBox(
-                        height: 40,
-                        child: TextField(
-                          // textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.black,
-                              textBaseline: TextBaseline.ideographic),
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              // labelText: 'Add new To Do',
-                              hintText: 'Add new To Do',
-                              contentPadding:
-                                  EdgeInsets.only(left: 10, bottom: 20)),
+            Visibility(
+              visible: show,
+              child: Container(
+                  margin: EdgeInsets.all(15),
+                  height: 120,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Flexible(
+                        fit: FlexFit.tight,
+                        child: SizedBox(
+                          height: 40,
+                          child: TextField(
+                            // textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.black,
+                                textBaseline: TextBaseline.ideographic),
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                // labelText: 'Add new To Do',
+                                hintText: 'Add new To Do',
+                                contentPadding:
+                                    EdgeInsets.only(left: 10, bottom: 20)),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 8,
-                      height: 40,
-                    ),
-                    SizedBox(
-                        width: 80,
+                      SizedBox(
+                        width: 8,
                         height: 40,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.green,
-                              foregroundColor: Colors.white),
-                          onPressed: () {},
-                          child: Text('Add'),
-                        ))
-                  ],
-                )),
+                      ),
+                      SizedBox(
+                          width: 80,
+                          height: 40,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.green,
+                                foregroundColor: Colors.white),
+                            onPressed: () {},
+                            child: Text('Add'),
+                          ))
+                    ],
+                  )),
+            ),
             Expanded(
               flex: 1,
               child: Container(
@@ -73,7 +77,11 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            show = true;
+          });
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
