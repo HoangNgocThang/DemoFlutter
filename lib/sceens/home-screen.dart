7b59baf2e6b81bd561e8183d4fcbee3188503a14
hoangngocthang <>
2023-02-13 09:00:24 +0700

    CM

--- lib/sceens/home-screen.dart
+++ lib/sceens/home-screen.dart
@@ -17,74 +17,80 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('To do List'),
+        title: const Text('To do List'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Visibility(
-              visible: show,
-              child: Container(
-                  margin: EdgeInsets.all(15),
-                  height: 120,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Flexible(
-                        fit: FlexFit.tight,
-                        child: SizedBox(
-                          height: 40,
-                          child: TextField(
-                            // textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.black,
-                                textBaseline: TextBaseline.ideographic),
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                // labelText: 'Add new To Do',
-                                hintText: 'Add new To Do',
-                                contentPadding:
-                                    EdgeInsets.only(left: 10, bottom: 20)),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 8,
-                        height: 40,
-                      ),
-                      SizedBox(
-                          width: 80,
-                          height: 40,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.green,
-                                foregroundColor: Colors.white),
-                            onPressed: () {},
-                            child: Text('Add'),
-                          ))
-                    ],
-                  )),
-            ),
+            buildFormAdd(),
             Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.amberAccent,
-              ),
-            ),
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: ListView(
+                    children: [
+                      Text("List To Do:"),
+                    ],
+                  ),
+                )),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            show = true;
+            show = !show;
           });
         },
+        backgroundColor: show ? Colors.black : Colors.lightBlue,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: show ? const Icon(Icons.close) : const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Visibility buildFormAdd() {
+    return Visibility(
+      visible: show,
+      child: Container(
+          // color: Colors.blue,
+          padding: const EdgeInsets.all(15),
+          height: 40,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Flexible(
+                fit: FlexFit.tight,
+                child: SizedBox(
+                  height: 40,
+                  child: TextField(
+                    // textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black,
+                        textBaseline: TextBaseline.ideographic),
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        // labelText: 'Add new To Do',
+                        hintText: 'Add new To Do',
+                        contentPadding: EdgeInsets.only(left: 10, bottom: 20)),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 8, height: 40),
+              SizedBox(
+                  width: 80,
+                  height: 40,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white),
+                    onPressed: () {},
+                    child: const Text('Add'),
+                  ))
+            ],
+          )),
     );
   }
 }
