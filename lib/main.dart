6744020ba914bb733a1c0f506482bd41ae18e970
hoangngocthang <>
2023-02-14 15:26:26 +0700

    cm

--- lib/main.dart
+++ lib/main.dart
@@ -14,48 +14,42 @@ int counterReducer(int state, dynamic action) {
   if (action == 'Increment') {
     return state + 1;
   }
-  if(action == 'Decrease') {
-     return state -1;
+  if (action == 'Decrease') {
+    return state - 1;
   }
   return state;
 }
 
-void main() {
-  final store = Store<int>(counterReducer, initialState: 0);
-  runApp(MyApp(store: store));
-}
+final store = Store<int>(counterReducer, initialState: 0);
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final Store<int> store;
-
-  const MyApp({super.key, required this.store});
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return 
-    StoreProvider(
-      store: store,
-      child: 
-      MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        routes: {
-          '/login': (context) => LoginScreen(),
-          '/home': (context) => HomeScreen(),
-          '/test': (context) => TestScreen(),
-          '/test-provider': (context) => TestProviderScreen(),
-          '/test-redux': (context) => TestReduxScreen()
-        },
-        initialRoute: '/test-redux',
-        debugShowCheckedModeBanner: false,
-        //  darkTheme: ThemeData.dark(),
-        // useInheritedMediaQuery: true,
-        // locale: DevicePreview.locale(context),
-        // builder: DevicePreview.appBuilder,
-      )
-    );
+    return StoreProvider(
+        store: store,
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          routes: {
+            '/login': (context) => LoginScreen(),
+            '/home': (context) => HomeScreen(),
+            '/test': (context) => TestScreen(),
+            '/test-provider': (context) => TestProviderScreen(),
+            '/test-redux': (context) => TestReduxScreen()
+          },
+          initialRoute: '/test-redux',
+          debugShowCheckedModeBanner: false,
+          //  darkTheme: ThemeData.dark(),
+          // useInheritedMediaQuery: true,
+          // locale: DevicePreview.locale(context),
+          // builder: DevicePreview.appBuilder,
+        ));
   }
 }
