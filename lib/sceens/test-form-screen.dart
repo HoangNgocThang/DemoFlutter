4e715e9a9524d83006a6eb8f11c42489ee36c162
HoangNgocThang <thanghai.tb123@gmail.com>
2023-03-01 16:01:07 +0700

    cm1

--- lib/sceens/test-form-screen.dart
+++ lib/sceens/test-form-screen.dart
@@ -17,79 +17,82 @@ class _TestFormScreenState extends State<TestFormScreen> {
     return Scaffold(
       appBar: AppBar(title: Text('Test Form Screen')),
       body: SafeArea(
-          child: Expanded(
-        flex: 1,
-        child: Column(children: [
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Name'),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: dateInput,
-                  decoration: InputDecoration(
-                      icon: Icon(Icons.calendar_today), //icon of text field
-                      labelText: "Enter Date" //label text of field
-                      ),
-                  readOnly: true,
+          child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Expanded(
+          flex: 1,
+          child: Column(children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Name'),
+                    // The validator receives the text that the user has entered.
                     validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some date';
-                    }
-                    return null;
-                  },
-                  onTap: () async {
-                    DateTime? pickedDate = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(1950),
-                        //DateTime.now() - not to allow to choose before today.
-                        lastDate: DateTime(2100));
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: dateInput,
+                    decoration: const InputDecoration(
+                        icon: Icon(Icons.calendar_today), //icon of text field
+                        labelText: "Enter Date" //label text of field
+                        ),
+                    readOnly: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some date';
+                      }
+                      return null;
+                    },
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1950),
+                          //DateTime.now() - not to allow to choose before today.
+                          lastDate: DateTime(2100));
 
-                    if (pickedDate != null) {
-                      print(
-                          pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                      String formattedDate =
-                          DateFormat('yyyy-MM-dd').format(pickedDate);
-                      print(
-                          formattedDate); //formatted date output using intl package =>  2021-03-16
-                      setState(() {
-                        dateInput.text =
-                            formattedDate; //set output date to TextField value.
-                      });
-                      // setState(() {
-                      //   dateInput.text = pickedDate.toString();
-                      // });
-                    }
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print(_formKey.currentState!.validate());
-                    // Validate returns true if the form is valid, or false otherwise.
-                    if (_formKey.currentState!.validate()) {
-                      // If the form is valid, display a snackbar. In the real world,
-                      // you'd often call a server or save the information in a database.
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
-                  },
-                  child: const Text('Submit'),
-                ),
-              ],
-            ),
-          )
-        ]),
+                      if (pickedDate != null) {
+                        print(
+                            pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+                        String formattedDate =
+                            DateFormat('yyyy-MM-dd').format(pickedDate);
+                        print(
+                            formattedDate); //formatted date output using intl package =>  2021-03-16
+                        setState(() {
+                          dateInput.text =
+                              formattedDate; //set output date to TextField value.
+                        });
+                        // setState(() {
+                        //   dateInput.text = pickedDate.toString();
+                        // });
+                      }
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print(_formKey.currentState!.validate());
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+                        // If the form is valid, display a snackbar. In the real world,
+                        // you'd often call a server or save the information in a database.
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ],
+              ),
+            )
+          ]),
+        ),
       )),
     );
   }
