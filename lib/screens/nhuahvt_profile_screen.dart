1875827168a515590064a4534822869a92b120c9
HoangNgocThang <thanghai.tb123@gmail.com>
2023-03-29 16:06:02 +0700

    Test Native module

--- lib/screens/nhuahvt_profile_screen.dart
+++ lib/screens/nhuahvt_profile_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NhuahvtProfileScreen extends StatefulWidget {
   const NhuahvtProfileScreen({super.key});
@@ -10,11 +13,38 @@ class NhuahvtProfileScreen extends StatefulWidget {
 }
 
 class _NhuahvtProfileScreenState extends State<NhuahvtProfileScreen> {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+  String _batteryLevel = 'Unknown battery level.';
+
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Profile"),
+      body: SafeArea(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: _getBatteryLevel,
+              child: const Text('Get Battery Level'),
+            ),
+            Text(_batteryLevel),
+          ],
+        ),
       ),
     );
   }
